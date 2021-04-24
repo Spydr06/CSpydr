fe4f3b445ce56ef03d71d082247e3f1342c6bb5c
Spydr06 <felix-wagenknecht@web.de>
2021-04-24 17:03:10 +0200

    Reimplemented the AST structs

---
+++ src/ast/ast.c
@@ -0,0 +1,447 @@
+#include "ast.h"
+#include "../io/log.h"
+
+#include <string.h>
+
+ASTRoot_T* initASTRoot(const char* filepath)
+{
+    ASTRoot_T* r = calloc(1, sizeof(struct AST_ROOT_STRUCT));
+
+    r->filepath = strdup(filepath);
+    r->functions = initList(sizeof(struct AST_FUCTION_STRUCT*));
+    r->globals = initList(sizeof(struct AST_GLOBAL_STRUCT*));
+    r->imports = initList(sizeof(struct AST_IMPORT_STRUCT*));
+    r->types = initList(sizeof(struct AST_TYPEDEF_STRUCT*));
+
+    return r;
+}
+
+void freeASTRoot(ASTRoot_T* r)
+{
+    free(r->filepath);
+
+    for(int i = 0; i < r->functions->size; i++)
+        freeASTFunction((ASTFunction_T*) r->functions->items[i]);
+    freeList(r->functions);
+
+    for(int i = 0; i < r->globals->size; i++)
+        freeASTGlobal((ASTGlobal_T*) r->globals->items[i]);
+    freeList(r->globals);
+
+    for(int i = 0; i < r->imports->size; i++)
+        freeASTImport((ASTImport_T*) r->imports->items[i]);
+    freeList(r->imports);
+
+    for(int i = 0; i < r->types->size; i++)
+        freeASTTypedef((ASTTypedef_T*) r->types->items[i]);
+    freeList(r->types);
+
+    free(r);
+}
+
+ASTImport_T* initASTImport(const char* file)
+{
+    ASTImport_T* i = calloc(1, sizeof(struct AST_IMPORT_STRUCT));
+    i->includeFile = strdup(file);
+    return i;
+}
+
+void freeASTImport(ASTImport_T* i)
+{
+    free(i->includeFile);
+    free(i);
+}
+
+ASTTypedef_T* initASTTypedef(ASTType_T* type, const char* name)
+{
+    ASTTypedef_T* t = calloc(1, sizeof(struct AST_TYPEDEF_STRUCT));
+    t->dataType = type;
+    t->name = strdup(name);
+    return t;
+}
+
+void freeASTTypedef(ASTTypedef_T* t)
+{
+    freeASTType(t->dataType);
+    free(t->name);
+    free(t);
+}
+
+ASTGlobal_T* initASTGlobal(const char* name, ASTType_T* type, ASTExpr_T* value)
+{
+    ASTGlobal_T* g = calloc(1, sizeof(struct AST_GLOBAL_STRUCT));
+    g->name = strdup(name);
+    g->type = type;
+    g->value = value;
+    return g;
+}
+
+void freeASTGlobal(ASTGlobal_T* g)
+{
+    free(g->name);
+    freeASTType(g->type);
+    freeASTExpr(g->value);
+    free(g);
+}
+
+ASTFunction_T* initASTFunction(const char* name, ASTType_T* returnType, ASTCompound_T* body)
+{
+    ASTFunction_T* f = calloc(1, sizeof(struct AST_FUCTION_STRUCT));
+    f->name = strdup(name);
+    f->body = body;
+    f->returnType = returnType;
+    return f;
+}
+
+void freeASTFunction(ASTFunction_T* f)
+{
+    freeASTCompound(f->body);
+    freeASTType(f->returnType);
+    free(f->name);
+    free(f);
+}
+
+ASTExprStmt_T* initASTExprStmt(ASTExpr_T* expr)
+{
+    ASTExprStmt_T* e = calloc(1, sizeof(struct AST_EXPR_STMT_STRUCT));
+    e->expr = expr;
+    return e;
+}
+
+void freeASTExprStmt(ASTExprStmt_T* e)
+{
+    freeASTExpr(e->expr);
+    free(e);
+}
+
+ASTMatch_T* initASTMatch(list_T* conditions, list_T* bodys, ASTCompound_T* defaultBody)
+{
+    ASTMatch_T* m = calloc(1, sizeof(struct AST_MATCH_STRUCT));
+    m->bodys = bodys;
+    m->conditions = conditions;
+    m->defaultBody = defaultBody;
+    return m;
+}
+
+void freeASTMatch(ASTMatch_T* m)
+{
+    for(int i = 0; i < m->conditions->size; i++)
+        freeASTExpr(m->conditions->items[i]);
+    freeList(m->conditions);
+
+    for(int i = 0; i < m->bodys->size; i++)
+        freeASTCompound(m->bodys->items[i]);
+    freeList(m->bodys);
+
+    if(m->defaultBody != NULL)
+        free(m->defaultBody);
+    
+    free(m);
+}
+
+ASTLocal_T* initASTLocal(ASTType_T* dataType, ASTExpr_T* value, const char* name)
+{
+    ASTLocal_T* l = calloc(1, sizeof(struct AST_LOCAL_STRUCT));
+    l->dataType = dataType;
+    l->value = value;
+    l->name = strdup(name);
+    return l;
+}
+
+void freeASTLocal(ASTLocal_T* l)
+{
+    free(l->name);
+    freeASTExpr(l->value);
+    freeASTType(l->dataType);
+    free(l);
+}
+
+ASTLoop_T* initASTLoop(ASTExpr_T* condition, ASTCompound_T* body)
+{
+    ASTLoop_T* l = calloc(1, sizeof(struct AST_LOOP_STRUCT));
+    l->body = body;
+    l->condition = condition;
+    return l;
+}
+
+void freeASTLoop(ASTLoop_T* l)
+{
+    freeASTCompound(l->body);
+    freeASTExpr(l->condition);
+    free(l);
+}
+
+ASTIf_T* initASTIf(ASTExpr_T* condition, ASTCompound_T* ifBody, ASTCompound_T* elseBody)
+{
+    ASTIf_T* i = calloc(1, sizeof(struct AST_If_STRUCT));
+    i->condition = condition;
+    i->ifBody = ifBody;
+    i->elseBody = elseBody;
+    return i;
+}
+
+void freeASTIf(ASTIf_T* i)
+{
+    freeASTCompound(i->ifBody);
+    freeASTExpr(i->condition);
+
+    if(i->elseBody != NULL) 
+        freeASTCompound(i->elseBody);
+    
+    free(i);
+}
+
+ASTReturn_T* initASTReturn(ASTType_T* type, ASTExpr_T* value)
+{
+    ASTReturn_T* r = calloc(1, sizeof(struct AST_RETURN_STRUCT));
+    r->value = value;
+    r->returnType = type;
+    return r;
+}
+
+void freeASTReturn(ASTReturn_T* r)
+{
+    freeASTExpr(r->value);
+    freeASTType(r->returnType);
+
+    free(r);
+}
+
+ASTStmt_T* initASTStmt(ASTStmtType_T type, void* stmt)
+{
+    ASTStmt_T* s = calloc(1, sizeof(struct AST_STATEMENT_STRUCT));
+    s->stmt = stmt;
+    s->type = type;
+    return s;
+}
+
+void freeASTStmt(ASTStmt_T* s)
+{
+    switch(s->type)
+    {
+        case STMT_EXPRESSION:
+            freeASTExprStmt(s->stmt);
+            break;
+        case STMT_IF:
+            freeASTIf(s->stmt);
+            break;
+        case STMT_LET:
+            freeASTLocal(s->stmt);
+            break;
+        case STMT_LOOP:
+            freeASTLoop(s->stmt);
+            break;
+        case STMT_RETURN:
+            freeASTReturn(s->stmt);
+            break;
+        case STMT_MATCH:
+            freeASTMatch(s->stmt);
+            break;
+        default:
+            LOG_ERROR_F("Undefined statement type %d\n", s->type);
+            break;
+    }
+    free(s);
+}
+
+ASTArray_T* initASTArray(list_T* exprs)
+{
+    ASTArray_T* a = calloc(1, sizeof(struct AST_ARRAY_LITERAL));
+    a->exprs = exprs;
+    a->len = a->exprs->size;
+    return a;
+}
+
+void freeASTArray(ASTArray_T* a)
+{
+    for(int i = 0; i < a->len; i++)
+        freeASTExpr(a->exprs->items[i]);
+    free(a);
+}
+
+ASTBool_T* initASTBool(bool _bool)
+{
+    ASTBool_T* b = calloc(1, sizeof(struct AST_BOOL_LITERAL));
+    b->_bool = _bool;
+    return b;
+}
+
+void freeASTBool(ASTBool_T* b)
+{
+    free(b);
+}
+
+ASTString_T* initASTString(const char* _string)
+{
+    ASTString_T* s = calloc(1, sizeof(struct AST_STRING_LITERAL));
+    s->_string = strdup(_string);
+    s->len = strlen(s->_string);
+    return s;
+}
+
+void freeASTString(ASTString_T* s)
+{
+    free(s->_string);
+    free(s);
+}
+
+ASTChar_T* initASTChar(char _char)
+{
+    ASTChar_T* c = calloc(1, sizeof(struct AST_CHAR_LITERAL));
+    c->_char = _char;
+    return c;
+}
+
+void freeASTChar(ASTChar_T* c)
+{
+    free(c);
+}
+
+ASTFloat_T* initASTFloat(float _float)
+{
+    ASTFloat_T* f = calloc(1, sizeof(struct AST_FLOAT_LITERAL));
+    f->_float = _float;
+    return f;
+}
+
+void freeASTFloat(ASTFloat_T* f)
+{
+    free(f);
+}
+
+ASTInt_T* initASTInt(int _int)
+{
+    ASTInt_T* i = calloc(1, sizeof(struct AST_INT_LITERAL));
+    i->_int = _int;
+    return i;
+}
+
+void freeASTInt(ASTInt_T* i)
+{
+    free(i);
+}
+
+ASTIdentifer_T* initASTIdentifier(const char* callee)
+{
+    ASTIdentifer_T* i = calloc(1, sizeof(struct AST_IDENTIFIER_STRUCT));
+    i->callee = strdup(callee);
+    return i;
+}
+
+void freeASTIdentifier(ASTIdentifer_T* i)
+{
+    free(i->callee);
+    free(i);
+}
+
+ASTPrefix_T* initASTPrefix(ASTPrefixOpType_T op, ASTExpr_T* right)
+{
+    ASTPrefix_T* p = calloc(1, sizeof(struct AST_PREFIX_STRUCT));
+    p->op = op;
+    p->right = right;
+    return p;
+}
+
+void freeASTPrefix(ASTPrefix_T* p)
+{
+    freeASTExpr(p->right);
+    free(p);
+}
+
+ASTInfix_T* initASTInfix(ASTInfixOpType_T op, ASTExpr_T* right, ASTExpr_T* left)
+{
+    ASTInfix_T* i = calloc(1, sizeof(struct AST_INFIX_STRUCT));
+    i->right = right; 
+    i->left = left;
+    i->op = op;
+    return i;
+}
+
+void freeASTInfix(ASTInfix_T* i)
+{
+    freeASTExpr(i->left);
+
+    if(i->right != NULL)
+        freeASTExpr(i->right);
+
+    free(i);
+}
+
+ASTExpr_T* initASTExpr(ASTType_T* dataType, ASTExprType_T type, void* ptr)
+{
+    ASTExpr_T* e = calloc(1, sizeof(struct AST_EXPRESSION_STRUCT));
+    e->type = type;
+    e->expr = ptr;
+    e->dataType = dataType;
+    return e;
+}
+
+void freeASTExpr(ASTExpr_T* e)
+{
+    freeASTType(e->dataType);
+
+    switch(e->type) {
+        case EXPR_PREFIX:
+            freeASTPrefix(e->expr);
+            break;
+        case EXPR_INFIX:
+            freeASTInfix(e->expr);
+            break;
+        case EXPR_IDENTIFIER:
+            freeASTIdentifier(e->expr);
+            break;
+        case EXPR_INT_LITERAL:
+            freeASTInt(e->expr);
+            break;
+        case EXPR_CHAR_LITERAL:
+            freeASTChar(e->expr);
+            break;
+        case EXPR_FLOAT_LITERAL:
+            freeASTFloat(e->expr);
+            break;
+        case EXPR_STRING_LITERAL:
+            freeASTString(e->expr);
+            break;
+        case EXPR_ARRAY_LITERAL:
+            freeASTArray(e->expr);
+            break;
+        case EXPR_BOOL_LITERAL:
+            freeASTBool(e->expr);
+            break;
+        default:
+            LOG_ERROR_F("Undefined epression ast type %d\n", e->type);
+            break;
+    }
+
+    free(e);
+}
+
+ASTCompound_T* initASTCompound(list_T* stmts)
+{
+    ASTCompound_T* c = calloc(1, sizeof(struct AST_COMPOUND_STRUCT));
+    c->stmts = stmts;
+    return c;
+}
+
+void freeASTCompound(ASTCompound_T* c)
+{
+    for(int i = 0; i < c->stmts->size; i++)
+        freeASTStmt(c->stmts->items[i]);
+
+    free(c);
+}
+
+ASTType_T* initASTType(ASTDataType_T type, ASTType_T* subtype)
+{
+    ASTType_T* t = calloc(1, sizeof(struct AST_TYPE_STRUCT));
+    t->type = type;
+    t->subtype = subtype;
+
+    return t;
+}
+
+void freeASTType(ASTType_T* t)
+{
+    freeASTType(t->subtype);
+    free(t);
+}
---
+++ src/ast/ast.h
@@ -0,0 +1,305 @@
+#ifndef CSPYDR_AST_H
+#define CSPYDR_AST_H
+
+#include "stdbool.h"
+
+#include "../list.h"
+
+typedef enum 
+{
+    AST_I32,
+    AST_I64,
+    AST_U32,
+    AST_U64,
+    AST_F32,
+    AST_F64,
+    AST_BOOL,
+    AST_CHAR,
+    AST_STRING,
+    AST_ARRAY,
+    AST_STRUCT,
+    AST_ENUM,
+} ASTDataType_T;
+
+typedef enum 
+{
+    STMT_LOOP,
+    STMT_IF,
+    STMT_RETURN,
+    STMT_LET,
+    STMT_EXPRESSION,
+    STMT_MATCH
+} ASTStmtType_T;
+
+typedef enum
+{
+    EXPR_PREFIX,
+    EXPR_INFIX,
+    EXPR_IDENTIFIER,
+    EXPR_INT_LITERAL,
+    EXPR_BOOL_LITERAL,
+    EXPR_FLOAT_LITERAL,
+    EXPR_CHAR_LITERAL,
+    EXPR_STRING_LITERAL,
+    EXPR_ARRAY_LITERAL,
+} ASTExprType_T;
+
+typedef enum
+{
+    OP_ADD,
+    OP_SUB,
+    OP_MULT,
+    OP_DIV,
+    OP_GT,
+    OP_LT,
+    OP_EQ,
+    OP_NOT_EQ,
+    OP_ASSIGN,
+    OP_RANGE,
+} ASTInfixOpType_T;
+
+typedef enum 
+{
+    OP_NOT,
+    OP_NEGATE,
+} ASTPrefixOpType_T;
+
+typedef struct AST_TYPE_STRUCT
+{
+    ASTDataType_T type;
+    struct AST_TYPE_STRUCT* subtype;
+} ASTType_T;
+
+ASTType_T* initASTType(ASTDataType_T type, ASTType_T* subtype);
+void freeASTType(ASTType_T* t);
+
+typedef struct AST_COMPOUND_STRUCT
+{
+    list_T* stmts;
+} ASTCompound_T;
+
+ASTCompound_T* initASTCompound(list_T* stmts);
+void freeASTCompound(ASTCompound_T* c);
+
+/////////////////////////////////
+// Expressions                 //
+/////////////////////////////////
+
+typedef struct AST_EXPRESSION_STRUCT
+{
+    ASTType_T* dataType;
+    ASTExprType_T type;
+    void* expr;
+} ASTExpr_T;
+
+ASTExpr_T* initASTExpr(ASTType_T* dataType, ASTExprType_T type, void* ptr);
+void freeASTExpr(ASTExpr_T* e);
+
+typedef struct AST_INFIX_STRUCT
+{
+    ASTInfixOpType_T op;
+
+    ASTExpr_T* left;
+    ASTExpr_T* right;
+} ASTInfix_T;
+
+ASTInfix_T* initASTInfix(ASTInfixOpType_T op, ASTExpr_T* right, ASTExpr_T* left);
+void freeASTInfix(ASTInfix_T* i);
+
+typedef struct AST_PREFIX_STRUCT
+{
+    ASTPrefixOpType_T op;
+
+    ASTExpr_T* right;
+} ASTPrefix_T;
+
+ASTPrefix_T* initASTPrefix(ASTPrefixOpType_T op, ASTExpr_T* right);
+void freeASTPrefix(ASTPrefix_T* p);
+
+typedef struct AST_IDENTIFIER_STRUCT
+{
+    char* callee;
+} ASTIdentifer_T;
+
+ASTIdentifer_T* initASTIdentifier(const char* callee);
+void freeASTIdentifier(ASTIdentifer_T* i);
+
+/////////////////////////////////
+// Literals                    //
+/////////////////////////////////
+
+typedef struct AST_INT_LITERAL
+{
+    int _int;
+} ASTInt_T;
+
+ASTInt_T* initASTInt(int _int);
+void freeASTInt(ASTInt_T* i);
+
+typedef struct AST_FLOAT_LITERAL
+{
+    float _float;
+} ASTFloat_T;
+
+ASTFloat_T* initASTFloat(float _float);
+void freeASTFloat(ASTFloat_T* f);
+
+typedef struct AST_CHAR_LITERAL
+{
+    char _char;
+} ASTChar_T;
+
+ASTChar_T* initASTChar(char _char);
+void freeASTChar(ASTChar_T* c);
+
+typedef struct AST_STRING_LITERAL
+{
+    char* _string;
+    unsigned int len;
+} ASTString_T;
+
+ASTString_T* initASTString(const char* _string);
+void freeASTString(ASTString_T* s);
+
+typedef struct AST_BOOL_LITERAL
+{   
+    bool _bool;
+} ASTBool_T;
+
+ASTBool_T* initASTBool(bool _bool);
+void freeASTBool(ASTBool_T* b);
+
+typedef struct AST_ARRAY_LITERAL
+{
+    list_T* exprs;
+    unsigned int len;
+} ASTArray_T;
+
+ASTArray_T* initASTArray(list_T* exprs);
+void freeASTArray(ASTArray_T* a);
+
+/////////////////////////////////
+// Statements                  //
+/////////////////////////////////
+
+typedef struct AST_STATEMENT_STRUCT
+{  
+    ASTStmtType_T type;
+    void* stmt;
+} ASTStmt_T;
+
+ASTStmt_T* initASTStmt(ASTStmtType_T type, void* stmt);
+void freeASTStmt(ASTStmt_T* s);
+
+typedef struct AST_RETURN_STRUCT
+{
+    ASTType_T* returnType;
+    ASTExpr_T* value;
+} ASTReturn_T;
+
+ASTReturn_T* initASTReturn(ASTType_T* type, ASTExpr_T* value);
+void freeASTReturn(ASTReturn_T* r);
+
+typedef struct AST_If_STRUCT
+{
+    ASTExpr_T* condition;
+    ASTCompound_T* ifBody;
+    ASTCompound_T* elseBody;   
+} ASTIf_T;
+
+ASTIf_T* initASTIf(ASTExpr_T* condition, ASTCompound_T* ifBody, ASTCompound_T* elseBody);
+void freeASTIf(ASTIf_T* i);
+
+typedef struct AST_LOOP_STRUCT
+{
+    ASTExpr_T* condition;
+
+    ASTCompound_T* body;
+} ASTLoop_T;
+
+ASTLoop_T* initASTLoop(ASTExpr_T* condition, ASTCompound_T* body);
+void freeASTLoop(ASTLoop_T* l);
+
+typedef struct AST_LOCAL_STRUCT
+{
+    ASTType_T* dataType;
+    ASTExpr_T* value;
+    char* name;
+} ASTLocal_T;
+
+ASTLocal_T* initASTLocal(ASTType_T* dataType, ASTExpr_T* value, const char* name);
+void freeASTLocal(ASTLocal_T* l);
+
+typedef struct AST_MATCH_STRUCT
+{
+    list_T* conditions;
+    list_T* bodys;
+    ASTCompound_T* defaultBody;
+} ASTMatch_T;
+
+ASTMatch_T* initASTMatch(list_T* conditions, list_T* bodys, ASTCompound_T* defaultBody);
+void freeASTMatch(ASTMatch_T* m);
+
+typedef struct AST_EXPR_STMT_STRUCT
+{
+    ASTExpr_T* expr;
+} ASTExprStmt_T;
+
+ASTExprStmt_T* initASTExprStmt(ASTExpr_T* expr);
+void freeASTExprStmt(ASTExprStmt_T* e);
+
+/////////////////////////////////
+// base structures             //
+/////////////////////////////////
+
+typedef struct AST_FUCTION_STRUCT
+{
+    char* name;
+    ASTCompound_T* body;
+    ASTType_T* returnType;
+} ASTFunction_T;
+
+ASTFunction_T* initASTFunction(const char* name, ASTType_T* returnType, ASTCompound_T* body);
+void freeASTFunction(ASTFunction_T* f);
+
+typedef struct AST_GLOBAL_STRUCT
+{
+    char* name;
+    ASTType_T* type;
+    ASTExpr_T* value;
+} ASTGlobal_T;
+
+ASTGlobal_T* initASTGlobal(const char* name, ASTType_T* type, ASTExpr_T* value);
+void freeASTGlobal(ASTGlobal_T* g);
+
+typedef struct AST_TYPEDEF_STRUCT
+{
+    ASTType_T* dataType;
+    char* name;
+} ASTTypedef_T;
+
+ASTTypedef_T* initASTTypedef(ASTType_T* type, const char* name);
+void freeASTTypedef(ASTTypedef_T* t);
+
+typedef struct AST_IMPORT_STRUCT
+{
+    char* includeFile;
+} ASTImport_T;
+
+ASTImport_T* initASTImport(const char* file);
+void freeASTImport(ASTImport_T* i);
+
+typedef struct AST_ROOT_STRUCT
+{
+    char* filepath;
+
+    list_T* imports;
+    list_T* types;
+    list_T* globals;
+    list_T* functions;
+} ASTRoot_T;
+
+ASTRoot_T* initASTRoot(const char* filepath);
+void freeASTRoot(ASTRoot_T* r);
+
+#endif
