01a2049e181e246a36aefcde4300bee0cb16eb86
Spydr06 <spydr06@web.de>
2022-03-12 15:50:45 +0100

    Added `generate.sh` scripts to generate repetitive parts of the stdlib automatically

---
+++ src/std/generate.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+NEWLINE=$'\n'
+
+function log() {
+    echo -e "[ SH ]\e[38;5;81m $1 \e[0m"
+}
+
+function gen-file() {
+    log "| generating $1"
+    echo $2 > $1
+}
+
+function gen-allimport-file() {
+    gen-file $1 "# $3"
+
+    echo "" >> $1
+
+    for file in *.csp; do
+        echo "import \"${file}\";" >> $1
+    done
+}
+
+pushd $SCRIPT_DIR
+    
+log "start generating stdlib"
+
+gen-allimport-file "std.csp" $SCRIPT_DIR "file for quickly importing all stdlib files."
+
+for dir in * ; do
+    if [ -d "${dir}" ] && [ -f "${dir}/generate.sh" ]; then
+        log "* $dir contains generate.sh"
+        source "$dir/generate.sh"
+    fi
+done
+
+log "stdlib generation finished"
+
+popd
---
+++ src/std/libc/generate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd $SCRIPT_DIR
+
+gen-allimport-file "libc.csp" $SCRIPT_DIR "file for quickly importing all libc files"
+
+popd
