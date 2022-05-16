b7cece0fee7731fd5cf9d26d1ce6389d86be2adf
Spydr06 <spydr06@web.de>
2022-05-16 22:01:37 +0200

    Added llvm generation for data types, globals and enums

--- src/compiler/codegen/llvm/llvm_codegen.c
+++ src/compiler/codegen/llvm/llvm_codegen.c
@@ -1,13 +1,22 @@
-#include <llvm-c/BitWriter.h>
+#include <llvm-c/Core.h>
+#include <llvm-c/Types.h>
 #ifdef CSPYDR_USE_LLVM
 
 #include <string.h>
+#include <stdarg.h>
 
+#include "list.h"
+#include "error/error.h"
+#include "mem/mem.h"
+#include "../codegen_utils.h"
 #include "ast/ast.h"
+#include "ast/ast_iterator.h"
 #include "io/log.h"
 #include "llvm_codegen.h"
 #include "llvm_includes.h"
 #include "globals.h"
+
+#define GET_GENERATOR(args) LLVMCodegenData_T* cg = va_arg(args, LLVMCodegenData_T*)
 
 typedef struct 
 {
@@ -19,11 +28,23 @@ typedef struct
     LLVMValueRef current_fn_value;
     ASTObj_T* current_fn;
     LLVMBasicBlockRef current_block;
+
+    LLVMValueRef* current_value;
 } LLVMCodegenData_T;
 
 static void optimize_llvm(LLVMCodegenData_T* cg);
 static char* llvm_emit_bc(LLVMCodegenData_T* cg, char* output_file);
 static void llvm_link_bc(LLVMCodegenData_T* cg, char* bc_file, char* output_file);
+
+static void gen_global(ASTObj_T* global, va_list args);
+static void gen_enum(ASTObj_T* tydef, va_list args);
+
+static ASTIteratorList_T iterator = {
+    .obj_end_fns = {
+        [OBJ_GLOBAL] = gen_global,
+        [OBJ_TYPEDEF] = gen_enum
+    }
+};
 
 static void init_llvm_codegen(LLVMCodegenData_T* cg, ASTProg_T* ast)
 {
@@ -56,6 +77,8 @@ void generate_llvm(ASTProg_T *ast, char *output_file, bool print_code, bool is_s
         exit(1);
     }
     LLVMDisposeMessage(error);
+
+    ast_iterate(&iterator, ast, &cg);
 
     if(global.optimize)
         optimize_llvm(&cg);
@@ -116,7 +139,164 @@ static char* llvm_emit_bc(LLVMCodegenData_T* cg, char* output_file)
 
 static void llvm_link_bc(LLVMCodegenData_T* cg, char* bc_file, char* output_file)
 {
+
+}
+
+static char* llvm_gen_identifier(LLVMCodegenData_T* cg, ASTIdentifier_T* id)
+{
+    char* str = gen_identifier(id, ".", false);
+    mem_add_ptr(str);
+    return str;
+}
+
+static LLVMTypeRef llvm_gen_type(LLVMCodegenData_T* cg, ASTType_T* type)
+{
+    type = unpack(type);
+    switch(type->kind)
+    {
+        case TY_I8:
+        case TY_U8:
+        case TY_BOOL:
+        case TY_CHAR:
+            return LLVMInt8Type();
+        case TY_I16:
+        case TY_U16:
+            return LLVMInt16Type();
+        case TY_I32:
+        case TY_U32:
+        case TY_ENUM:
+            return LLVMInt32Type();
+        case TY_I64:
+        case TY_U64:
+            return LLVMInt64Type();
+        case TY_F32:
+            return LLVMFloatType();
+        case TY_F64:
+            return LLVMDoubleType();
+        case TY_F80:
+            return LLVMFP128Type();
+        case TY_VOID:
+            return LLVMVoidType();
+        case TY_PTR:
+        case TY_VLA:
+            return LLVMPointerType(llvm_gen_type(cg, type->base), 8);
+        case TY_C_ARRAY:
+            return LLVMArrayType(llvm_gen_type(cg, type->base), type->num_indices);
+        case TY_FN:
+            {
+                LLVMTypeRef* params = calloc(type->arg_types->size, sizeof(LLVMTypeRef));
+                for(size_t i = 0; i < type->arg_types->size; i++)
+                {
+                    ASTType_T* ty = type->arg_types->items[i];
+                    params[i] = llvm_gen_type(cg, ty);
+                }
+                mem_add_ptr(params);
+
+                return LLVMPointerType(
+                    LLVMFunctionType(
+                        llvm_gen_type(cg, type->base), 
+                        params, 
+                        type->arg_types->size, 
+                        type->is_variadic
+                    ), 8
+                );
+            }
+        case TY_STRUCT:
+            {
+                LLVMTypeRef* elements = calloc(type->members->size, sizeof(LLVMTypeRef));
+                for(size_t i = 0; i < type->members->size; i++)
+                {
+                    ASTNode_T* member = type->members->items[i];
+                    elements[i] = llvm_gen_type(cg, member->data_type);
+                }
+                mem_add_ptr(elements);
+
+                return LLVMStructType(elements, type->members->size, false);    // todo: union support
+            }
+        
+        case TY_ARRAY:
+            {
+                LLVMTypeRef* elements = calloc(2, sizeof(LLVMTypeRef));
+                mem_add_ptr(elements);
+                elements[0] = LLVMInt64Type();
+                elements[1] = LLVMArrayType(llvm_gen_type(cg, type->base), type->num_indices);
+
+                return LLVMStructType(elements, 2, false); // arrays are two parts: the size and the data
+            }
+        
+        case TY_UNDEF:
+        case TY_TYPEOF:
+        case TY_TEMPLATE:
+        case TY_KIND_LEN:
+            unreachable();
+            break;
+    }
+    return LLVMInt32Type();
+}
+
+static LLVMValueRef gen_global_initializer(LLVMCodegenData_T* cg, ASTNode_T* node, LLVMTypeRef type)
+{
+    switch(node->kind)
+    {
+        case ND_NIL:
+            return LLVMConstNull(type);
+        case ND_STR:
+            return LLVMConstString(node->str_val, strlen(node->str_val), false);
+        case ND_CHAR:
+            return LLVMConstInt(type, node->int_val, false);
+        case ND_BOOL:
+            return LLVMConstInt(type, node->bool_val, false);
+        case ND_INT:
+            return LLVMConstInt(type, node->int_val, true);
+        case ND_LONG:
+            return LLVMConstInt(type, node->long_val, true);
+        case ND_ULONG:
+            return LLVMConstInt(type, node->ulong_val, false);
+        case ND_FLOAT:
+            return LLVMConstReal(type, node->float_val);
+        case ND_DOUBLE:
+            return LLVMConstReal(type, node->double_val);
+        case ND_CLOSURE:
+            return gen_global_initializer(cg, node->expr, type);
+        case ND_CAST:
+            return gen_global_initializer(cg, node->left, llvm_gen_type(cg, node->data_type));
+        case ND_NEG:
+            return LLVMConstNeg(gen_global_initializer(cg, node->left, type));
+        
+        case ND_ARRAY:
+        case ND_STRUCT:
+            throw_error(ERR_CODEGEN, node->tok, "not implemented");
+
+        default:
+            throw_error(ERR_CODEGEN, node->tok, "cannot generate relocation for `%s` (%d)", node->tok->value, node->kind);
+    }
+
+    return LLVMConstNull(type);
+}
+
+static void gen_global(ASTObj_T* global, va_list args)
+{
+    GET_GENERATOR(args);
+    LLVMTypeRef type = llvm_gen_type(cg, global->data_type);
+    LLVMValueRef value = LLVMAddGlobal(cg->module, type, llvm_gen_identifier(cg, global->id));
+    if(!global->is_extern && should_emit(global))
+        LLVMSetInitializer(value, gen_global_initializer(cg, global->value, type));
+}
+
+static void gen_enum(ASTObj_T* tydef, va_list args)
+{
+    GET_GENERATOR(args);
+
+    if(!tydef->data_type || tydef->data_type->kind != TY_ENUM)
+        return;
+    LLVMTypeRef type = LLVMInt32Type();
     
+    for(size_t i = 0; i < tydef->data_type->members->size; i++)
+    {
+        ASTObj_T* member = tydef->data_type->members->items[i];
+        LLVMValueRef value = LLVMAddGlobal(cg->module, type, llvm_gen_identifier(cg, member->id));
+        LLVMSetInitializer(value, gen_global_initializer(cg, member->value, type));
+    }
 }
 
 #endif
