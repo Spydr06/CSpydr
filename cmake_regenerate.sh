c9006a53250093a8c65ba32a7849179b363247fc
Spydr06 <spydr06@web.de>
2022-07-01 17:40:48 +0200

    Add a small script for easy cmake regenerating, update `doc` submodule

---
+++ cmake_regenerate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# A small script to delete the CMake cache and regenerate the project
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+pushd $SCRIPT_DIR
+
+rm CMakeCache.txt
+rm *.cmake
+rm -rf bin
+rm -rf .cache
+
+cmake . "$@"
+
+popd
